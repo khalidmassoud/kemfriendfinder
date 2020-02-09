54451a7476253c0febd0f39dcd7d39e95e537fe3
khalidmassoud <khalid.massoud@hotmail.com>
2020-02-09 17:04:27 -0500

    updated sending frient

---
+++ app/data/friends.js
@@ -0,0 +1,208 @@
+var friends =[
+{
+    "name": "Gisele",
+    "photo": "https://asset2.modelmanagement.com/mm-eyJ0Ijp7InIiOnsibCI6/IjE2MDAiLCJoIjoiMTIw/MCJ9LCJ3Ijp7InR4Ijoi/RWxpbmEgTWl0cm9mYW5v/dmFcbm1vZGVsbWFuYWdl/bWVudC5jb21cL21vZGVs/XC9lbGluYS1taXRyb2Zh/bm92YSIsInR4byI6eyJs/IjoiODAwIiwiaCI6IjEy/MDAifX0sIjAiOnsidyI6/InNmbSJ9fSwiaWQiOiJp/Mjc3NjgwOSIsImYiOiJq/cGcifQ;;.jpg",
+    "scores": [
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+
+    ],
+  
+
+},
+
+{
+    "name": "Tania",
+    "photo": "http://icetoday.net/wp-content/uploads/2017/01/Top-Models-10-649x1024.jpg",
+    "scores": [
+        3,
+        4,
+        2,
+        1,
+        5,
+        2,
+        3,
+        4,
+        1,
+        1
+
+    ],
+  
+    
+
+},
+
+{
+    "name": "Hellen",
+    "photo": "https://i.pinimg.com/736x/9c/1f/02/9c1f02546d00f98a3ee06d9846afeb6b--female-models-top-models.jpg",
+     "scores": [
+        3,
+        1,
+        4,
+        3,
+        4,
+        2,
+        3,
+        4,
+        5,
+        5
+
+    ],
+  
+
+},
+
+{
+    "name": "Celine",
+    "photo": "https://www.brandedgirls.com/wp-content/uploads/2016/03/15-Ayten-Asli-Enver.jpg",
+     "scores": [
+        2,
+        1,
+        1,
+        4,
+        3,
+        3,
+        1,
+        3,
+        2,
+        5
+
+    ],
+  
+
+},
+
+
+{
+    "name": "Laura",
+    "photo": "http://www.brandedgirls.com/wp-content/uploads/2016/03/6-Nadine-Aghnatios.jpg",
+     "scores": [
+        2,
+        4,
+        4,
+        4,
+        4,
+        5,
+        2,
+        3,
+        1,
+        2
+
+    ],
+  
+
+},
+
+
+{
+    "name": "Jennifer",
+    "photo": "https://i.pinimg.com/736x/9c/1f/02/9c1f02546d00f98a3ee06d9846afeb6b--female-models-top-models.jpg",
+     "scores": [
+        3,
+        2,
+        1,
+        1,
+        5,
+        4,
+        2,
+        3,
+        5,
+        4
+
+    ],
+  
+
+},
+
+
+{
+    "name": "Leila",
+    "photo": "https://images.complex.com/complex/images/c_fill,g_center,w_1200/fl_lossy,pg_1,q_auto/nukvogyqv7cf2a3z4qux/rachel-weisz",
+     "scores": [
+        5,
+        4,
+        2,
+        1,
+        3,
+        2,
+        1,
+        2,
+        4,
+        3
+
+    ],
+  
+
+},
+
+{
+    "name": "Ursula",
+    "photo": "http://www1.pictures.gi.zimbio.com/Celebrities+Attend+Top+Glamour+Women+Year+5UhSzn8AIY5x.jpg",
+     "scores": [
+        2,
+        1,
+        4,
+        4,
+        3,
+        3,
+        3,
+        2,
+        2,
+        2
+
+    ],
+  
+
+},
+
+{
+    "name": "Paris",
+    "photo": "https://thumbs.dreamstime.com/z/bossy-woman-13883684.jpg",
+     "scores": [
+        3,
+        2,
+        4,
+        5,
+        5,
+        5,
+        2,
+        3,
+        5,
+        5
+
+    ],
+  
+
+},
+
+{
+    "name": "Susan",
+    "photo": "http://www.beautiful-women-pedia.com/images/xleonor-varela-pictures1.jpg.pagespeed.ic_.LWWqh5GghI.jpg",
+     "scores": [
+        3,
+        2,
+        2,
+        2,
+        5,
+        2,
+        1,
+        1,
+        1,
+        1
+
+    ],
+  
+
+},
+]
+
+module.exports = friends;
+  
